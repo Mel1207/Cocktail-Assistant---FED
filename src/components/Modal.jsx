2258b202936404e13404973dd7e9398ab0b5711a
Mel <57514727+Mel1207@users.noreply.github.com>
2024-07-08 17:00:28 +0800

    added modal fix ui

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import { useGlobalStore } from '../store/globalStore'
 import { useCartStore } from '../store/cartStore'
 import iconClose from '../assets/icon-close.svg'
+import iconCheck from '../assets/icon-check.svg'
+import iconEmpty from '../assets/icon-empty.svg'
 
 const Modal = () => {
   const { closeModal } = useGlobalStore()
   const { cart } = useCartStore()
+
+  console.log(cart.length)
 
   return (
     <div className='modal-container'>
@@ -18,20 +22,38 @@ const Modal = () => {
             </button>
           </div>
           <div className="modal-body">
-            <h3>Hi Mel here’s your current cart</h3>
-            <p>If all your items are correct please proceed to payments</p>
+            {cart.length === 0 ? (
+              <div className='empty-cart'>
+                <img src={iconEmpty} alt="empty cart" />
+                <p>you cart is empty</p>
+              </div>
+              
+            ) : (
+              <>
+                <h3>Hi Mel here’s your current cart</h3>
+                <p>If all your items are correct please proceed to payments</p>
 
-            <ul>
-              {cart.map(item => (
-                <li>{item}</li>
-              ))}
-            </ul>
+                <ul className='modal-cart-list'>
+                  {cart.map((item, index) => (
+                    <li key={index}>
+                      <img src={iconCheck} alt="checked icon" />
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </>
+            )}
+            
           </div>
         </div>
        
         <div className="modal-footer">
-          <button onClick={ closeModal }>Close</button>
-          <button onClick={ closeModal } className='btn btn-primary'>Go to payments</button>
+          <button onClick={ closeModal } className='btn-secondary'>Close</button>
+          {cart.length === 0 ? (
+            <button className='btn btn-disabled'>Go to payments</button>
+          ) : (
+            <button onClick={ closeModal } className='btn btn-primary'>Go to payments</button>
+          )}
         </div>
       </div> 
     </div>
